329ae26651069079f485fe2ee8f69d87dc9dd00c
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-10-30 20:40:41 +0100

    added illustrations.co

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,23 @@
-import '../styles/globals.css'
+import localFont from '@next/font/local'
+import { Azeret_Mono } from '@next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+// const carosRegular = localFont({
+//   src: '../assets/Caros.otf',
+//   variable: '--caros-regular'
+//  })
+
+//  console.log('mee')
+
+//  const ubuntu = Azeret_Mono({ 
+//   weight: '400',
+//   subsets: ['latin'],
+//   variable: '--oxygen-font'
+// })
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
