5924b8414712be01af6a4d17bd00fb6f29b06901
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-11-19 16:45:04 +0100

    changed font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,6 @@
-import localFont from '@next/font/local'
-import { Azeret_Mono } from '@next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
-
-// const carosRegular = localFont({
-//   src: '../assets/Caros.otf',
-//   variable: '--caros-regular'
-//  })
-
-//  console.log('mee')
-
-//  const ubuntu = Azeret_Mono({ 
-//   weight: '400',
-//   subsets: ['latin'],
-//   variable: '--oxygen-font'
-// })
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
