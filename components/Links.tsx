a22f563d9c45f08c370116e4fb51ea220219a4b7
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2022-11-27 21:23:13 +0100

    added icons to links

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,39 +1,40 @@
 import Link from "next/link";
+import Image from 'next/image'
 import { useRouter } from "next/router";
-import styles from '../styles/Links.module.css'
+import styles from '../styles/Links.module.css';
 
 const navLinks = [
   {
     name: 'Design',
     items: [
-      { title: 'Colors', path: '/design/colors' },
-      { title: 'Typography', path: '/design/fonts' },
-      { title: 'Games', path: '/design/games' },
-      { title: 'Icons', path: '/design/icons' },
-      { title: 'Design Systems', path: '/design/systems' },
-      { title: 'Illustrations', path: '/design/illustrations' },
-      { title: 'Stock Photos', path: '/design/stock-photos' },
-      { title: 'Best Practices', path: '/design/best-practices' },
-      { title: 'Inspiration', path: '/design/inspiration' },
-      { title: 'Tools', path: '/design/tools' },
-      { title: 'AI Art', path: '/design/ai-art' },
+      { title: 'Colors', path: '/design/colors', icon: 'colors' },
+      { title: 'Typography', path: '/design/fonts', icon: 'fonts' },
+      { title: 'Games', path: '/design/games', icon: 'games' },
+      { title: 'Icons', path: '/design/icons', icon: 'icons' },
+      { title: 'Design Systems', path: '/design/systems', icon: 'design-systems' },
+      { title: 'Illustrations', path: '/design/illustrations', icon: 'illustrations' },
+      { title: 'Stock Photos', path: '/design/stock-photos', icon: 'stock-photos' },
+      { title: 'Best Practices', path: '/design/best-practices', icon: 'best-practices' },
+      { title: 'Inspiration', path: '/design/inspiration', icon: 'ideas' },
+      { title: 'Tools', path: '/design/tools', icon: 'tools' },
+      { title: 'AI Art', path: '/design/ai-art', icon: 'ai-art' },
     ]
   },
   {
     name: 'Productivity',
     items: [
-      { title: 'Tools', path: '/productivity/tools' },
-      { title: 'Books', path: '/productivity/books' },
+      { title: 'Tools', path: '/productivity/tools', icon: 'tools' },
+      { title: 'Books', path: '/productivity/books', icon: 'books' },
     ]
   },
   {
     name: 'Coding',
     items: [
-      { title: 'Books', path: '/coding/books' },
-      { title: 'Tools', path: '/coding/tools' },
-      { title: 'Practice', path: '/coding/practice' },
-      { title: 'Project Ideas', path: '/coding/ideas' },
-      { title: 'VS-Code Extensions', path: '/coding/vscode-extensions' },
+      { title: 'Books', path: '/coding/books', icon: 'books' },
+      { title: 'Tools', path: '/coding/tools', icon: 'tools' },
+      { title: 'Practice', path: '/coding/practice', icon: 'practice' },
+      { title: 'Project Ideas', path: '/coding/ideas', icon: 'ideas' },
+      { title: 'VS-Code Extensions', path: '/coding/vscode-extensions', icon: 'vs-code-extensions' },
     ]
   }
 ]
@@ -49,12 +50,13 @@ const Links = () => {
           return (
             <div key={bucket.name}>
               <h4 className={styles.category}>{bucket.name}</h4>
+
               {
                 bucket.items.map(link => {
                   return (
                     <div className={styles.link_wrapper} key={link.title}>
                       <Link key={link.title} href={link.path} passHref className={styles.a + ' ' + (router.asPath === link.path ? styles.active : "")}>
-                        {link.title}
+                      <Image src={router.asPath === link.path ? '/icons/' + link.icon + '-white.svg' : '/icons/' + link.icon + '.svg'} width={24} height={24} alt="" /> {link.title}
                       </Link>
                     </div>
                   )
