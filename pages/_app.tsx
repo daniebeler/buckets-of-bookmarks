a7c4696220f44d8fd5e2969afe2b011d9b2720be
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-01-05 21:31:58 +0100

    implemented next/font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Rubik } from '@next/font/google'
+
+const rubikLight = Rubik({
+  subsets: ['latin'],
+  weight: '400',
+  display: 'swap'
+})
+
+const rubikMedium = Rubik({
+  subsets: ['latin'],
+  weight: '500',
+  display: 'swap'
+})
+
+const rubikBold = Rubik({
+  subsets: ['latin'],
+  weight: '700',
+  display: 'swap'
+})
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        * {
+          font-family: ${rubikLight.style.fontFamily};
+        }
+
+        h1 {
+          font-family: ${rubikBold.style.fontFamily};
+        }
+
+        h2, h3, h4 {
+          font-family: ${rubikMedium.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
