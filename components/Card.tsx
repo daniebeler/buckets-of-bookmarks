b850cb457061f4b91b87f4c230d8b499faeecd2c
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2024-10-13 22:03:06 +0200

    add screenshots

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,87 +9,107 @@ const Card = (props: any) => {
         href={props.url}
         rel="noreferrer"
       >
-        <div className="flex flex-row">
-          <div className="pr-2 pb-2">
+        <div className="flex flex-col w-full items-center h-full">
+          <div className="pb-2">
             <Image
               src={"/images/" + props.img}
               alt=""
-              width={80}
-              height={80}
-              className="rounded-lg"
+              width={40}
+              height={40}
+              className="rounded-full"
             />
           </div>
-          <div>
+
+          
+
+          <h2 className="text-white text-lg text-center">{props.name}</h2>
+          <p className="text-gray-300 text-sm pt-1 text-center">
+            {props.description}
+          </p>
+
+          <div className="flex pt-1">
             {props.tags &&
               props.tags.map((item: any) => {
                 if (item.name === "free") {
                   return (
-                    <div key={item.name} className="text-xs text-green-500">
+                    <div key={item.name} className="text-xs text-green-500 p-1">
                       FREE
                     </div>
                   );
                 } else if (item.name === "os") {
                   return (
-                    <div key={item.name} className="text-xs text-gray-400">
+                    <div key={item.name} className="text-xs text-gray-400 p-1">
                       OPEN SOURCE
                     </div>
                   );
                 } else if (item.name === "freemium") {
                   return (
-                    <div key={item.name} className="text-xs text-orange-500">
+                    <div key={item.name} className="text-xs text-orange-500 p-1">
                       FREEMIUM
                     </div>
                   );
                 } else if (item.name === "paid") {
                   return (
-                    <div key={item.name} className="text-xs text-red-500">
+                    <div key={item.name} className="text-xs text-red-500 p-1">
                       PAID
                     </div>
                   );
                 } else if (item.name === "rss") {
                   return (
-                    <div key={item.name} className="text-xs text-yellow-500">
+                    <div key={item.name} className="text-xs text-yellow-500 p-1">
                       RSS
                     </div>
                   );
                 } else if (item.name === "pwa") {
                   return (
-                    <div key={item.name} className="text-xs text-purple-600">
+                    <div key={item.name} className="text-xs text-purple-600 p-1">
                       PWA
                     </div>
                   );
                 } else if (item.name === "android") {
                   return (
-                    <div key={item.name} className="text-xs text-green-500">
+                    <div key={item.name} className="text-xs text-green-500 p-1">
                       ANDROID
                     </div>
                   );
                 } else if (item.name === "ios") {
                   return (
-                    <div key={item.name} className="text-xs text-gray-500">
+                    <div key={item.name} className="text-xs text-gray-500 p-1">
                       IOS
                     </div>
                   );
                 } else if (item.name === "tailwind") {
                   return (
-                    <div key={item.name} className="text-xs text-blue-500">
+                    <div key={item.name} className="text-xs text-blue-500 p-1">
                       TAILWIND
                     </div>
                   );
                 } else if (item.name === "react") {
                   return (
-                    <div key={item.name} className="text-xs text-blue-500">
+                    <div key={item.name} className="text-xs text-blue-500 p-1">
                       REACT
                     </div>
                   );
                 }
               })}
           </div>
+
+          <div className="flex-1">
+
+          </div>
+
+          {props.screenshot && (
+            <img
+              src={"/images/" + props.screenshot}
+              alt=""
+              className="rounded-lg w-full aspect-video object-cover mt-4"
+            />
+          )}
         </div>
-        <div>
+        {/*  <div>
           <h2 className="text-white text-lg">{props.name}</h2>
           <p className="text-gray-300 text-sm pt-1">{props.description}</p>
-        </div>
+        </div> */}
       </a>
     </div>
   );
--- components/Collection.tsx
+++ components/Collection.tsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 const Collection = (props: any) => {
   return (
     <div className="p-4">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {props.data.map((item: any) => {
           return (
             <div key={item.name}>
@@ -12,6 +12,7 @@ const Collection = (props: any) => {
                 description={item.description}
                 url={item.url}
                 img={item.img}
+                screenshot={item.screenshot}
                 tags={item.tags}
               />
             </div>
